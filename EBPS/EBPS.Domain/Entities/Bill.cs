7af63b58406e7d6c8a6033c4be0efdae0d805ba6
AduwuJoseph <aduwujoseph4real@gmail.com>
2024-10-30 01:32:55 +0100

    Created a Domain class library where I defined the entity models

---
+++ EBPS/EBPS.Domain/Entities/Bill.cs
@@ -0,0 +1,23 @@
+﻿using EBPS.Contract.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBPS.Domain.Entities
+{
+    public class Bill
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Range(0, 900000000000, MinimumIsExclusive = false, MaximumIsExclusive = false)]
+        public decimal Amount { get; set; }
+        [AllowedValues("A", "B")]
+        [Required]
+        public string Providers { get; set; }   
+        public string Status { get; set; } = BillStatus.Pending.ToString(); // Pending or Paid
+    }
+
+}
---
+++ EBPS/EBPS.Domain/Entities/Wallet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBPS.Domain.Entities
+{
+    public class Wallet
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public decimal Balance { get; set; }
+
+        [EmailAddress]
+        public string Email {  get; set; }
+    }
+
+}
